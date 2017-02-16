0f878da926355dcdf3ebb264ac0ccf6390f8d6a6
Bijiabo <bijiabo@gmail.com>
2017-02-16 14:07:44 +0800

    start

---
+++ app/index.js
@@ -0,0 +1,24 @@
+/**
+ * Created by huchunbo on 2017/2/10.
+ */
+import {test, Point, switchCell} from './../components/test';
+import '../style/basic.scss';
+
+var $ = require('jquery');
+
+$(function(){
+    console.log($('body'));
+    console.log(test);
+    test.appendElement();
+    console.log(test);
+
+    let point = new Point(1, 1);
+    console.log(point.toString());
+
+    global.Point = Point;
+    global.UI = { switchCell, };
+
+    let powerCell = new UI.switchCell('power', {
+        title: 'power'
+    });
+});
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+/**
+ * Created by huchunbo on 2017/2/10.
+ */
+var path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './app/index.js',
+    externals: {
+        jquery: 'window.$'
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer:{inline:true},
+    module: {
+        loaders: [
+            { test: /\.js$/, exclude: /node_modules/, loader: ['babel-loader'] }
+        ],
+        rules: [{
+            test: /\.scss$/,
+            use: [{
+                loader: "style-loader"
+            }, {
+                loader: "css-loader", options: {
+                    sourceMap: true
+                }
+            }, {
+                loader: "sass-loader", options: {
+                    sourceMap: true,
+                    includePaths: ["node_modules"]
+                }
+            }]
+        }]
+    },
+    // eslint: {
+    //     failOnWarning: false,
+    //     failOnError: true
+    // },
+    plugins: [
+        new webpack.LoaderOptionsPlugin({
+            test: /\.js$/,
+            enforce: 'pre',
+            exclude: /node_modules/,
+            options: {
+                eslint: {
+                    /* your eslint loader config */
+                    configFile: './.eslintrc'
+                }
+            }
+        })
+    ],
+    eslint: {
+        formatter: require('eslint-friendly-formatter'),
+        failOnWarning: false,
+        failOnError: true
+    },
+};
