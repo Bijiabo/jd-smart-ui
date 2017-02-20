070a016d50dd615f8c357ca027f292908b89ba39
SecretCastle <chenfangzhong@hotmail.com>
2017-02-20 09:43:15 +0800

    update

--- app/index.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by huchunbo on 2017/2/10.
- */
-// import {test, Point, switchCell} from './../components/test';
-import '../style/basic.scss';
-import UI from '../component/core';
-
