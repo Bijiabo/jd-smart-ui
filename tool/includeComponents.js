5d18c0fa7c2eb9a6ba702bda911d53e3db357ddf
Bijiabo <bijiabo@gmail.com>
2017-02-23 16:35:07 +0800

    优化输出

--- tool/includeComponents.js
+++ tool/includeComponents.js
@@ -9,26 +9,30 @@ const files = fs.readdirSync(dir);
 
 var fileNameArray = [];
 var includeFileScript = '// 此文件为自动生成，不需要手动修改@@。\n\n';
+
+console.log('\nstart loading app/components \n');
+
 files.forEach(function (filename) {
-    // var fullname = path.join(dir,filename);
-    console.log(filename);
-    const modelName = filename.replace(/\.js$/,'');
-    includeFileScript += `import ${modelName} from './components/${modelName}';\n`;
-    fileNameArray.push(modelName);
-    // var stats = fs.statSync(fullname);
-    // if (stats.isDirectory()) filename += '/';
-    // process.stdout.write(filename + '\t' +
-    //     stats.size + '\t' +
-    //     stats.mtime + '\n'
-    // );
+    var fullname = path.join(dir,filename);
+    var stats = fs.statSync(fullname);
+    if (!stats.isDirectory()) {
+        console.log(filename);
+        const modelName = filename.replace(/\.js$/,'');
+        includeFileScript += `import ${modelName} from './components/${modelName}';\n`;
+        fileNameArray.push(modelName);
+    }
 });
 
 includeFileScript += `\nexport default {${fileNameArray.join(', ')}};`;
 
-console.log(includeFileScript);
+console.log(`
+===================== Start ======================
+${includeFileScript}
+=====================  End  ======================
+`);
 
 var includeFilePath = path.join(__dirname, './../app/componentList.js');
 fs.writeFile(includeFilePath, includeFileScript, (err) => {
     if (err) throw err;
-    console.log('It\'s saved!');
+    console.log('写入文件完成 ^_^');
 });
