ee899561761771d937bc9752444f28dbbafe4244
SecretCastle <chenfangzhong@hotmail.com>
2017-03-14 14:19:31 +0800

    修改配置

--- webpack.config.js
+++ webpack.config.js
@@ -16,38 +16,41 @@ module.exports = {
     resolve: {
         extensions: ['.js', '.jsx', '.css', '.scss']
     },
-    devServer:{inline:true},
+    devServer: { inline: true },
     module: {
-        loaders: [
-            {test: /\.js$/, loaders: [ "babel-loader", "eslint-loader" ], exclude: /node_modules/},
-        ],
         rules: [{
+            test: /\.js$/,
+            loaders: ["babel-loader", "eslint-loader"],
+            exclude: /node_modules/
+        }, {
             test: /\.scss$/,
             use: [{
                 loader: "style-loader"
             }, {
-                loader: "css-loader", options: {
+                loader: "css-loader",
+                options: {
                     sourceMap: true
                 }
             }, {
-                loader: "sass-loader", options: {
+                loader: "sass-loader",
+                options: {
                     sourceMap: true,
                     includePaths: ["node_modules"]
                 }
             }]
-        },{
-                enforce: 'pre',
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: [{
-                    loader: 'eslint-loader', 
-                    query: {
-                        configFile: './.eslintrc'
-                    },
-                }],
+        }, {
+            enforce: 'pre',
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: [{
+                loader: 'eslint-loader',
+                query: {
+                    configFile: './.eslintrc'
+                },
+            }],
         }]
     },
-    plugins:[
+    plugins: [
         new webpack.BannerPlugin('This file is created by Bijiabo and SecretCastle')
     ]
 };
